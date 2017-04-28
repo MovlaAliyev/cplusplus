a5fa7fa321c9a3b60bbde167814d924db5e7013e
movla_aliyev <eliyevmovlan@gmail.com>
2017-04-28 09:45:10 +0400

    Add files via upload

---
+++ PrimerExercises/CH1/Exercise1.5/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Enter two numbers:" << std::endl;
+    int x = 0, y = 0;
+
+    std::cin >> x >> y;
+    std::cout << "The sum of " << x ;
+
+    std::cout << " and " << y << " is ";
+    std::cout << x + y << std::endl;
+
+  return 0;
+}
