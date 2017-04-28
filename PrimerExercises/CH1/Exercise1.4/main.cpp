06a699773d5440bd88b2e04fecd5741da0bca606
movla_aliyev <eliyevmovlan@gmail.com>
2017-04-28 09:35:17 +0400

    Add files via upload

---
+++ PrimerExercises/CH1/Exercise1.4/main.cpp
@@ -0,0 +1,16 @@
+/*
+Our program used the addition operator, +, to add two
+numbers. Write a program that uses the multiplication operator, *, to print
+the product instead.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int v1 = 2;
+    int v2 = 3;
+    cout << "answer = " << v1 * v2 << endl;
+    return 0;
+}
