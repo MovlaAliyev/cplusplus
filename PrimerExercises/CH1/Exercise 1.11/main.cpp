e90551874efa1054f9abd647fe08d53465b1a582
movla_aliyev <eliyevmovlan@gmail.com>
2017-04-28 10:04:15 +0400

    Add files via upload

---
+++ PrimerExercises/CH1/Exercise 1.11/main.cpp
@@ -0,0 +1,21 @@
+/*
+Write a program that prompts the user for two integers.
+Print each number in the range specified by those two integers.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int from, to;
+
+    cout << "Enter 2 number: ";
+    cin >> from >> to;
+
+    while(from <= to)
+        cout << from++ << " ";
+
+
+    return 0;
+}
