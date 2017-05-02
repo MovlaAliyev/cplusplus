0415fca1cedc3ec650f489a5e6bb2eb83d0b546e
movla_aliyev <eliyevmovlan@gmail.com>
2017-05-02 10:53:25 +0400

    Add files via upload

---
+++ PrimerExercises/CH3/Exercise3.18/main.cpp
@@ -0,0 +1,20 @@
+/*
+List three ways to define a vector and give it ten elements,
+each with the value 42. Indicate whether there is a preferred way to do so
+and why.
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    vector<int> v1(10,42);
+    vector<int> v2 {42, 42, 42, 42, 42, 42, 42, 42, 42, 42};
+    vector<int> v3;
+
+    for(int i = 0; i < 10; i++)
+        v3.push_back(42);
+
+    return 0;
+}
