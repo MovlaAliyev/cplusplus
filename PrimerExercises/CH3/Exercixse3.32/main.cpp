5a907bf6166aa82717465951919278134e9213ad
movla_aliyev <eliyevmovlan@gmail.com>
2017-05-03 15:23:46 +0400

    Add files via upload

---
+++ PrimerExercises/CH3/Exercixse3.32/main.cpp
@@ -0,0 +1,28 @@
+/*
+Copy the array you defined in the previous exercise into
+another array. Rewrite your program to use vectors.
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+    int arr[10];
+    int arrTwo[10];
+
+    for(int i = 0; i < 10; i++)  arr[i]    = i;
+
+    for(int i = 0; i < 10; i++)  arrTwo[i] = arr[i];
+
+    vector<int> v1;
+
+    for(int i = 0; i < 10; i++)  v1.push_back(i);
+
+    vector<int> v2(v1);
+
+
+
+    return 0;
+}
