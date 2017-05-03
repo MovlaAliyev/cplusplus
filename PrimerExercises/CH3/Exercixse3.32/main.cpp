a8dbe40248c805bec74fa2514ab2d442be16b6df
movla_aliyev <eliyevmovlan@gmail.com>
2017-05-03 15:24:20 +0400

    Update main.cpp

--- PrimerExercises/CH3/Exercixse3.32/main.cpp
+++ PrimerExercises/CH3/Exercixse3.32/main.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 int main()
 {
-
     int arr[10];
     int arrTwo[10];
 
@@ -21,8 +20,6 @@ int main()
     for(int i = 0; i < 10; i++)  v1.push_back(i);
 
     vector<int> v2(v1);
-
-
 
     return 0;
 }
