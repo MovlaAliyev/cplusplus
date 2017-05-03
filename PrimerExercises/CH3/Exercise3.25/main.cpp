e1aea63183873033823177bcdc7f3750aa9ba52b
movla_aliyev <eliyevmovlan@gmail.com>
2017-05-03 14:09:08 +0400

    Add files via upload

--- PrimerExercises/CH3/Exercise3.25/main.cpp
+++ PrimerExercises/CH3/Exercise3.25/main.cpp
@@ -1,9 +1,19 @@
+/*
+Rewrite the grade clustering program from § 3.3.3 (p. 104)
+using iterators instead of subscripts.
+*/
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::vector<unsigned> scores(11, 0);
+    unsigned grade;
+    while (std::cin >> grade)
+        ++*(scores.begin() + grade / 10);
+    for (const auto & elem : scores)
+        std::cout << elem << ' ';
+    std::cout << std::endl;
     return 0;
 }
